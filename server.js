b254dd70dc032b4efa31d8dbe4b379d5a4008403
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-05-29 19:13:06 -0500

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const uuid = require('uuid')
 const util = require('util')
 const path = require('path')
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 

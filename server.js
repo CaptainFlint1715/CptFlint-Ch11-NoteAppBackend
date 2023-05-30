6ab785e961e743dc5b7dcb063ea5d93702056720
CaptainFlint1715 <chanseshirley123@gmail.com>
2023-05-29 22:04:44 -0500

    reworked get routes, added to fetch functions in index, moved * get route to bottom

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const uuid = require('uuid')
 const util = require('util')
 const path = require('path')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -33,6 +33,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 
+// GET Route for homepage
+
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
 app.get('/assets/js/index.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'assets', 'js', 'index.js'));
   });
@@ -47,57 +55,63 @@ app.get('/assets/js/index.js', (req, res) => {
   });
 
 
-// GET Route for homepage
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'index.html'))
-);
-
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'notes.html'))
-);
-
-
-
-
-
 // GET Route for retrieving all previously saved notes
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for saved notes`);
+  
+    readFromFile('./db/db.json', 'utf8')
+      .then((data) => {
+        console.log('File contents:', data); // Log the contents of the file
+        res.json(JSON.parse(data));
+      })
+      .catch((error) => {
+        console.error('Error reading file:', error);
+        res.status(500).json({ error: 'Failed to read notes data.' });
+      });
+  });
 
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
 
-
-// POST Route for saving a new note
-app.post('/api/notes', (req, res) => {
+  app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to save note`);
-
+  
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-
+  
     // If all the required properties are present
     if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-            noteID: uuid(),
-        };
-
-        readAndAppend(newNote, './db/db.json');
-
-        const response = {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        noteID: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      // Fetch the updated list of notes
+      readFromFile('./db/db.json', 'utf8')
+        .then((data) => {
+          const notes = JSON.parse(data);
+          const response = {
             status: 'success',
             body: newNote,
-        };
-
-        res.json(response);
+            notes: notes, // Include the updated notes in the response
+          };
+          res.json(response);
+        })
+        .catch((error) => {
+          console.error('Error reading file:', error);
+          res.status(500).json({ error: 'Failed to read notes data.' });
+        });
     } else {
-        res.json('Error in posting note');
+      res.status(400).json({ error: 'Missing required properties.' });
     }
-});
+  });
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
